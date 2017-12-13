6211c56a4f279e14e713536591c7b089a1d80f02
leto22 <wellingthon.monteiro10@gmail.com>
2017-12-13 13:31:59 -0300

    ViewModel

---
+++ ProjetoDiscoteca.Musicas.Web/ViewModels/Album/AlbumIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoDiscoteca.Musicas.Web.ViewModels.Album
+{
+    public class AlbumIndexViewModel
+    {
+        public int AlbumID { get; set; }
+
+        [Display(Name = "Ano do Álbum")]
+        public int Ano { get; set; }
+
+        [Display(Name ="Nome do Álbum")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrições")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Email de Contato")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
